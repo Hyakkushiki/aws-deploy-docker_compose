e005a0072f75205641bb31a95dd10e9ff83bbaa9
CZ <c.zander@vwall.co.jp>
2022-06-17 18:12:00 +0900

    init

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>I changed this!</p>
+        <p>2 for 3?</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React | and Docker
         </a>
       </header>
     </div>
